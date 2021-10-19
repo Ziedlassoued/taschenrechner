29bbf37bf61a7e09670ca1097f69f1b0d6a1f3c4
ziedlassoued <zied.lassoued@outlook.de>
2021-10-19 07:47:07 +0200

    Add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function appendOperation(appendOperation) {
+  document.getElementById("resultArea").innerHTML += appendOperation;
+}
+function calculateResult() {
+  let container = document.getElementById("resultArea");
+  container.innerHTML = "";
+}
